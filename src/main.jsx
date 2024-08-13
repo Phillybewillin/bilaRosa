121c93f57cc9333d1c48090632f62156e1995b89
kissyfaceDev <markphiloyaro27@gmail.com>
2024-08-13 14:49:02 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
+import ErrorBoundary from './js/pages/Errorboundary.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-        <BrowserRouter>
+    <ErrorBoundary>
+       <BrowserRouter>
         <App />
         </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
