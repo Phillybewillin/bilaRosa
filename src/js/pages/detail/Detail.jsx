c4207fa9e26e2cea41875d94ad0baab8056d9f45
phillybewillin <markphiloyaro27@gmail.com>
2024-08-28 22:13:04 +0300

    Update Detail.jsx

--- src/js/pages/detail/Detail.jsx
+++ src/js/pages/detail/Detail.jsx
@@ -33,7 +33,7 @@ const Detail = () => {
         const getDetail = async () => {
             const response = await tmdbApi.detail(category, id, {params: {}});
             setItem(response);
-           
+            window.scrollTo(0, 0);
            // console.log(response);
         }
         getDetail();
@@ -203,13 +203,13 @@ const Detail = () => {
         const navigate = useNavigate();
   
         const handlePlayer = (itemId, itemName) => {
-          //console.log('handlePlayer function called', itemId, itemName);
+          console.log('handlePlayer function called', itemId, itemName);
 
           if (itemName && itemId) {
               const encodedTitle = encodeURIComponent(itemName.replace(/ /g, '-').toLowerCase());
               console.log(`Navigating to: /watch/${encodedTitle}/${itemId}`);
               navigate(`/watch/${encodedTitle}/${itemId}`);
-              //console.log(itemId, itemName);
+              console.log(itemId, itemName);
           }
       };
 
@@ -219,9 +219,9 @@ const Detail = () => {
                 item && (
                     <>
                     <div className="movie-content__poste" style={{backgroundImage: `url(${apiConfig.w200Image(item.backdrop_path ? item.backdrop_path : item.poster_path)})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', transition: 'filter 0.5s ease'}}>
-                    <div className="banner" loading="lazy" style={{backgroundImage: `url(${apiConfig.originalImage(item.backdrop_path ? item.backdrop_path : item.poster_path)})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', transition: 'filter 0.5s ease'}}></div>
+                    <div className="banner" loading="lazy" style={{backgroundImage: `url(${apiConfig.originalImage(item.backdrop_path ? item.backdrop_path : item.poster_path)})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat',position: 'fixed', top: '0' , left: '0' , width: '100%' , height: '100vh'}}></div>
                     </div> 
-                       <div className="mb-3 movie-content container">
+                       <div className="detail-container">
                            <div className="movie-content__poster" style={{ backgroundImage: `url(${apiConfig.w200Image(item.poster_path ? item.poster_path : item.backdrop_path)})`, backgroundSize: 'cover', backgroundPosition: 'center',backgroundRepeat: 'no-repeat',borderRadius: '10px'}} >
                                <div className="movie-content__poster__img" loading="lazy" style={{ backgroundImage: `url(${apiConfig.w1280Image(item.poster_path ? item.poster_path : item.backdrop_path)})`}}>
                                </div>
@@ -238,7 +238,7 @@ const Detail = () => {
                                     ))
                                 }
                                </div>
-                              <p onClick={saveShow} style={{position : 'absolute', backgroundColor : 'black' , padding : '3px' ,borderRadius : '5px', top : '4px' , right : '5px' , cursor : 'pointer' , color : like ? 'red' : 'white'}}>
+                              <p onClick={saveShow} style={{position : 'absolute', backgroundColor : 'white' , padding : '5px' ,borderRadius : '5px', top : '2px' , right : '5px' , cursor : 'pointer' , color : like ? 'red' : 'black'}}>
                                      {like ? <i className='bx bxs-bookmark-plus'  style={{fontSize :'17px'}}></i> :<i className='bx bx-bookmark-plus' style={{fontSize :'17px'}}></i> }
                                </p>
                                <div className="secz">
@@ -253,8 +253,8 @@ const Detail = () => {
                               <div className="release-date"><i className="bx bx-calendar"></i> {item.release_date || item.first_air_date}</div>
                             <div className="language"><i className="bx bx-world"></i> {item.original_language.toUpperCase()}</div>
                                </div>
-                               <div className="overview">
-                                <h4>Overview</h4>
+                               <div className="overviewz">
+                                <h4 className='titleoverview'>Overview</h4>
                                  <p className="overview">{item.overview}</p>
                                  </div>
                                
@@ -289,12 +289,6 @@ const Detail = () => {
                                <MovieList category={category} type="similar" id={item.id}/>
                            </div>
                        
-                       <div className="container">
-                           <div className="section-video">
-                               <VideoList category={category} id={item.id}/>
-                           </div>
-                          
-                       </div>
                        <ToastContainer theme='dark' position="top-right" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss={false} draggable={false} pauseOnHover={false} backdrop={true} progressStyle={{ backgroundColor: '#ff0000' , color : 'white', borderRadius : '5px'}}/>
            
                     </>
