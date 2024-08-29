a6255c00a2f9fd7afcb25258c243d720dc582499
phillybewillin <markphiloyaro27@gmail.com>
2024-08-29 10:02:19 +0300

    Update MovieCard.jsx

--- src/js/components/movie-card/MovieCard.jsx
+++ src/js/components/movie-card/MovieCard.jsx
@@ -73,7 +73,7 @@ const MovieCard = (props) => {
         const threeWeeksAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate() - (5 * 7));
         if (props.category === 'movie') {
             if (releaseDate < today) {
-                return releaseDate > threeWeeksAgo ? '.In Cinema' : '';
+                return releaseDate > threeWeeksAgo ? ' • In Cinema' : '';
             }
             else{
                 return '.Coming soon';
@@ -93,6 +93,26 @@ const MovieCard = (props) => {
       };
       img.src = apiConfig.w500Image(item.poster_path || item.backdrop_path);
     }, [item]);
+
+    const voteAverage = item.vote_average; // example vote average
+    const votePercentage = voteAverage * 10; // convert to percentage
+
+    const getColor = (votePercentage) => {
+      if (votePercentage >= 86) {
+          return 'magenta';
+      } else if (votePercentage >= 70) {
+          return 'rgb(9, 255, 0)';
+      } 
+      else if (votePercentage >= 55) {
+          return 'yellow';
+      } else {
+        return 'red';
+      }
+          
+     
+
+  }
+    //console.log(`${votePercentage}%`); // output: 74%
   
     return (
         <>
@@ -103,36 +123,37 @@ const MovieCard = (props) => {
             <Skeleton baseColor="#161616d6" variant="rectangular"  className="movie-card" width={'100%'} height={'100%'} />
           </SkeletonTheme>
         ) : (
-          <div className="movie-card" loading="lazy" onClick={() => handlecardClick(item.id, props.category ,item.title || item.name, item.poster_path, )} style={{backgroundImage : `url(${bg})`}}>
+          <div className="movie-card" loading="lazy" onClick={() => handlecardClick(item.id, props.category ,item.title || item.name, item.poster_path, )} style={{backgroundImage : `url(${bg})`, backgroundSize : 'cover' , backgroundPosition : 'center' , backgroundRepeat : 'no-repeat'}}>
             <div className="inCinema">
-              <i class='bx bx-badge-check' style={{fontSize :'18px'}}></i>
-              {cinemaStatus}
+              <i className='bx bx-badge-check' style={{fontSize :'13px'}}></i> {cinemaStatus}
             </div>
+            <div className="catz">{props.category }</div> 
             <div className="btnz" style={{fontSize : '20px'}}>
               <i className="bx bx-play"></i>
             </div>
-          </div> 
-        )}
-      </Link>
-          
-          <div loading="lazy"  className="infomovie">
+            <div loading="lazy"  className="infomovie">
             <div className="title">
               <div className="titlename">
                 {item.title || item.name || <Skeleton count={1}/>}
               </div> 
               <div className="savemovie">
-                 <p onClick={saveShow} style={{cursor : 'pointer' , color : saved ? 'white' : 'black'}}>
+                 <p onClick={saveShow} style={{cursor : 'pointer' , color : saved ? 'red' : 'rgba(255, 255, 255, 0.549)'}}>
           {saved ? <i class='bx bxs-bookmark-plus'  style={{fontSize :'19px'}}></i> :<i class='bx bx-bookmark-plus' style={{fontSize :'18px'}}></i>}
            </p>
               </div>
            
             </div>
-            <div className="cat"> <div className="catz">{props.category || <Skeleton count={1}/>}</div>
+            <div className="cat"> 
             <h4 className='year'>
-               {Number.isNaN(year) ? '' : year}
+            • {Number.isNaN(year) ? '' : year}
            </h4>
-                <div className="vote">{item.vote_average}</div> </div>
+                <div className="vote" style={{color: getColor(votePercentage.toFixed(0))}}>{votePercentage.toFixed(0)}%</div> </div>
           </div>
+          </div> 
+        )}
+      </Link>
+          
+         
            </div>
         </>
     );
