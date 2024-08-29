9d49aec26321b31c1bb48fb8f2e7c06afb388c48
phillybewillin <markphiloyaro27@gmail.com>
2024-08-29 22:16:09 +0300

    Update Footer.jsx

--- src/js/components/footer/Footer.jsx
+++ src/js/components/footer/Footer.jsx
@@ -5,21 +5,8 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
 
     return (
-        <div className="footer">
-                <div className="footer__content__logo">
-                   <div className="logo">
-                       <Link to="/">Made by some guy</Link>
-                        <i className="bx bxs-heart" style={{color: 'white', fontSize: '16px' ,paddingLeft: "7px"}}></i>
-                    </div>
-                    <div className="acca">
-                    <a href="https://www.buymeacoffee.com/zillaxr.xyz" target="_blank" rel="noreferrer"><i className="bx bx bxs-coffee" style={{color: 'pink', fontSize: '25px' ,backgroundColor:"black", padding:"5px" ,borderRadius:"5px"}}></i></a>
-                  
-                    <a href="https://t.me/+MQUUqEx2WXA0ZmZk" target="_blank" rel="noreferrer"><i className="bx bxl-telegram" style={{color: 'white', fontSize: '25px',padding : '5px',backgroundColor : 'aqua',borderRadius : '5px' ,marginLeft:"5px"}}></i></a>
-                    <a href="https://reddit.com/r/zillaXRxyz" target="_blank " rel="noreferrer"><i className="bx bxl-reddit"style={{color: 'white',fontSize: '25px',padding : '5px',backgroundColor : 'red',borderRadius : '5px',marginLeft:"5px"}}></i></a>
-                    
-                    </div>
-                   
-                </div>
+        <div className="footer" loading="lazy">
+               
                 <div className="footer__content__menus">
                 <div className="footer__content__menu">
               
@@ -31,19 +18,46 @@ This site does not store any files on our server, we only linked to the media wh
                       <div className="links">
                         <Link to="/">Home</Link>
                        <div className="splitter"></div>
-                       <Link to="/socials">Pivacy Policy</Link>
+                       <Link to="/filter">Filters</Link>
+                        <div className="splitter"></div>
+                        <Link to="/movie">Movies</Link>
+                        <div className="splitter"></div>
+                        <Link to="/tv">TV Series</Link>
                        </div>
+                       
                        
                    </div>
                     
                     <div className="footer__content__menu">
                     <div className="links">
-                    <Link to="/movie">Must Watch</Link>
+                        <Link to="/dmca">DMCA</Link>
                         <div className="splitter"></div>
-                        <Link to="/account">Watch Later</Link>
+                        <Link to="/contact">Contact</Link>
+                        <div className="splitter"></div>
+                        <Link to="/info">About</Link>
+                        <div className="splitter"></div>
+                        <Link to="/socials">Privacy Policy</Link>
                          </div>
                        </div>
                 </div>
+                <div className="footer__content__logo">
+                   <div className="logo">
+                       <Link to="/">Made by some guy</Link>
+                       <i className='bx bxs-basketball bx-spin' style={{color: 'white', fontSize: '15px' ,paddingLeft: "7px"}}></i>
+                    </div>
+                    <div className="xilla"><h1>ZILLA-XR</h1></div>
+                    <div className="acca">
+                    <a href="https://buymeacoffee.com/zillaxr.xyz" target="_blank" rel="noreferrer"><i className='bx bxs-donate-heart' style={{color: 'pink', fontSize: '25px' ,backgroundColor:"black", padding:"5px" ,borderRadius:"5px" ,marginLeft:"5px"}}></i></a>
+
+                    
+                    <a href="https://t.me/+MQUUqEx2WXA0ZmZk" target="_blank" rel="noreferrer"><i className="bx bxl-telegram" style={{color: 'white', fontSize: '25px',padding : '5px',backgroundColor : 'black',borderRadius : '5px' ,marginLeft:"5px"}}></i></a>
+                    <a href="https://reddit.com/r/zillaXRxyz" target="_blank " rel="noreferrer"><i className="bx bxl-reddit"style={{color: 'white',fontSize: '25px',padding : '5px',backgroundColor : 'black',borderRadius : '5px',marginLeft:"5px"}}></i></a>
+                    <a href="https://x.com/ZillaXRxyz" target="_blank" rel="noreferrer"><i className="bx bxl-twitter" style={{color: 'aqua', fontSize: '25px' ,backgroundColor:"black", padding:"5px" ,borderRadius:"5px" ,marginLeft:"5px"}}></i></a>
+                  
+                    </div>
+                   
+                </div>
+                <div className="acc"><i className=" bx bx copyright" style={{color: 'white', fontSize: '11px' ,paddingLeft: "7px"}}> </i> <h5>© zilla-xr.xyz</h5> <h6>@2024.</h6></div>
             </div>
     );
 }
