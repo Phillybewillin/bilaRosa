438241426b82ad01067c84267589ecf967172a41
phillybewillin <markphiloyaro27@gmail.com>
2024-09-04 04:57:21 +0300

    Update Detail.jsx

--- src/js/pages/detail/Detail.jsx
+++ src/js/pages/detail/Detail.jsx
@@ -22,22 +22,29 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import MovieCard from '../../components/movie-card/MovieCard';
 
 const Detail = () => {
       
     const { category, id } = useParams();
 
     const [item, setItem] = useState(null);
+    const [items, setItems] = useState([]);
      
     const getDetail = async () => {
       const response = await tmdbApi.detail(category, id, {params: {}});
+      const similar = await tmdbApi.similar(category, id )
+
+      setItems(similar)
+
       setItem(response);
-      window.scrollTo(0, 0);
-     // console.log(response);
+     //console.log(similar);
   }
-    useEffect(() => {
-        getDetail();
-    }, [category, id]);
+  useEffect(() => {
+    document.title =  item ? `${item?.title || item?.name} - Watch on ZillaXR` : 'ZillaXR';
+    getDetail();
+    //console.log('items:', items);
+  }, [category, id , item]);
 
   
 
@@ -147,49 +154,7 @@ const Detail = () => {
     const [like , setLike] = useState(false);
    
    
-         
-        // Function to set the iframe src based on the item type
-
-        useEffect(() => {
-            const blurDivs = document.querySelectorAll('.movie-content__poster');
-            blurDivs.forEach(div => {
-                const img = div.querySelector('.movie-content__poster__img');
-                const originalImageUrl = apiConfig.originalImage(item.backdrop_path ? item.backdrop_path : item.poster_path);
-        
-                function loaded() {
-                    div.classList.add('loaded');
-                    div.classList.remove('w200');
-                    img.style.backgroundImage = `url(${originalImageUrl})`;
-                }
-        
-                if (img.complete) {
-                    loaded();
-                } else {
-                    img.addEventListener('load', loaded);
-                }
-            })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, []);
-        useEffect(() => {
-            const blurDivs = document.querySelectorAll('.movie-content__poste');
-            blurDivs.forEach(div => {
-                const img = div.querySelector('.banner');
-                const originalImageUrl = apiConfig.originalImage(item.backdrop_path ? item.backdrop_path : item.poster_path);
-        
-                function loaded() {
-                    div.classList.add('loaded');
-                    div.classList.remove('w200');
-                    img.style.backgroundImage = `url(${originalImageUrl})`;
-                }
-        
-                if (img.complete) {
-                    loaded();
-                } else {
-                    img.addEventListener('load', loaded);
-                }
-            })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, []);
+  
         
         const navigate = useNavigate();
   
@@ -203,6 +168,9 @@ const Detail = () => {
               console.log(itemId, itemName);
           }
       };
+      const releaseYear = item?.release_date || item?.first_air_date;
+      const year = (new Date(releaseYear)).getFullYear();
+    
       const voteAverage = item?.vote_average;
       const votePercentage = voteAverage * 10; // convert to percentage
     const getColor = (votePercentage) => {
@@ -218,25 +186,38 @@ const Detail = () => {
       }
     };
 
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    };
+
     return (
         <>
             {
                 item && (
                     <>
-                    <div className="movie-content__poste" style={{backgroundImage: `url(${apiConfig.w200Image(item.backdrop_path ? item.backdrop_path : item.poster_path)})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', transition: 'filter 0.5s ease'}}>
-                    <div className="banner" loading="lazy" style={{backgroundImage: `url(${apiConfig.originalImage(item.backdrop_path ? item.backdrop_path : item.poster_path)})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat',position: 'fixed', top: '0' , left: '0' , width: '100%' , height: '100vh'}}></div>
-                    </div> 
+                    
                        <div className="detail-container">
+                       <>
+                    <div className="banner" loading="lazy" style={{backgroundImage: `url(${apiConfig.originalImage(item.backdrop_path ? item.backdrop_path : item.poster_path)})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat',position: 'absolute', top: '0' , left: '0' , width: '100%' , height: '55vh'}}></div>
+                    </>
                         <div className="movie-content">
-                        <div className="movie-content__poster" style={{ backgroundImage: `url(${apiConfig.w200Image(item.poster_path ? item.poster_path : item.backdrop_path)})`, backgroundSize: 'cover', backgroundPosition: 'center',backgroundRepeat: 'no-repeat',borderRadius: '10px'}} >
-                               <div className="movie-content__poster__img" loading="lazy" style={{ backgroundImage: `url(${apiConfig.w1280Image(item.poster_path ? item.poster_path : item.backdrop_path)})`}}>
-                               </div>
-                           </div>
-
                            <div className="movie-content__info">
+                            <div className="titleholder">
                                <h2 className="title">
                                    {item.title || item.name}
+                                  
                                </h2>
+                               <div className="language"><i className="bx bx-world" style={{fontSize:'11px'}}></i> {item.original_language.toUpperCase()}</div>
+                               <div className="language">• {Number.isNaN(year) ? '' : year}</div>
+                                <div className="language"><i className="bx bx-time" style={{fontSize:'11px'}}></i>{item.runtime || item.episode_run_time} min</div>
+                            
+                            </div>
+                              
+          
+                               <div className="overviewz">
+                                
+                                 <p className="overviewz">{item.overview}</p>
+                                 </div>
                                <div className="genres" >
                                 {
                                     item.genres && item.genres.slice(0, 5).map((genre, i) => (
@@ -244,29 +225,19 @@ const Detail = () => {
                                     ))
                                 }
                                </div>
-                              <p onClick={saveShow} style={{position : 'absolute', backgroundColor : 'white' , padding : '5px' ,borderRadius : '5px', top : '2px' , right : '5px' , cursor : 'pointer' , color : like ? 'red' : 'black'}}>
+                               <div className="rating" style={{color: getColor(votePercentage.toFixed(0))}}>{votePercentage.toFixed(0)}%</div>
+                               <p onClick={saveShow} style={{ position:'absolute', bottom:'0' ,right:'0',cursor : 'pointer' , color : like ? 'red' : 'black'}}>
                                      {like ? <i className='bx bxs-bookmark-plus'  style={{fontSize :'17px'}}></i> :<i className='bx bx-bookmark-plus' style={{fontSize :'17px'}}></i> }
                                </p>
-                               <div className="secz">
-                                <div className="runtime"><i className="bx bx-time"></i>{item.runtime || item.episode_run_time} min</div>
-                                <div className="rating" style={{color: getColor(votePercentage.toFixed(0))}}>{votePercentage.toFixed(0)}%</div>
-      
-                              <div className="release-date"><i className="bx bx-calendar"></i> {item.release_date || item.first_air_date}</div>
-                            <div className="language"><i className="bx bx-world"></i> {item.original_language.toUpperCase()}</div>
-                               </div>
-                               <div className="overviewz">
-                                <h4 className='titleoverview'>Overview</h4>
-                                 <p className="overview">{item.overview}</p>
-                                 </div>
+                            
                                
                               {category === 'tv' && <Seasons category={category}
                               id={item.id} title={item.name || item.title}/>
                               }
                               {category === 'movie' && 
                               <>
-                              <div className="playbtn" onClick={() => handlePlayer(item.id, item.name || item.title)}><i className='bx bx-play-circle' style={{color: 'white', fontSize: '50px'}}></i>Play</div>
-                        
-                        <div className="butz">
+                              
+                        <div className="playbtn">
 
                         <Button className="btn" onClick={() => handlePlayer(item.id, item.name || item.title)}>Watch Now</Button>
                       </div>
@@ -277,19 +248,36 @@ const Detail = () => {
                                  
                            </div>
                         </div>
+
+                        <div className="castdiv">
+                        <h4 className='titledetailz'>Cast</h4>
+
+                                       <div className="castdix">
+                                           <p className='cast'><CastList id={item.id}/></p>
+                                            
+                                       </div>
+                                       
+                        </div>
                            
                        </div> 
-                       <div className="overview">
-                                       <div className="overview">
-                                           <h4 className='titledetails'>Casts</h4>
-                                       </div>
-                                       <CastList id={item.id}/> 
+                       <div className="overviewz">
+                                   <h3 className='titledetails'>Recommendations based on {item.title || item.name}</h3>
+                      </div>
+                       <div className="wrappwez">
+                              
+                               {
+                   items.results.map((itemz ,ia) =>(
+                    
+                            <div className="wrappwezs" key={ ia} onClick={scrollToTop}>
+                            
+                            <MovieCard item={itemz} category={category} key={itemz.id}/>
+                            {itemz.overview && <p className="overviewz">{itemz.overview}</p>}
                         </div>
-                       <div className="section mb-3">
-                               <div className="overview">
-                                   <h3 className='titledetails'>You might also like</h3>
-                               </div>
-                               <MovieList category={category} type="similar" id={item.id}/>
+                        
+                        
+                        
+                    ))
+                }
                            </div>
                        
                        <ToastContainer theme='dark' position="top-right" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss={false} draggable={false} pauseOnHover={false} backdrop={true} progressStyle={{ backgroundColor: '#ff0000' , color : 'white', borderRadius : '5px'}}/>
