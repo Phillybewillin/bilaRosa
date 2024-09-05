c1d7685f4befa96f134d107b72c83fe1537d06a5
phillybewillin <markphiloyaro27@gmail.com>
2024-09-05 22:38:31 +0300

    Add files via upload

--- src/js/pages/Search.jsx
+++ src/js/pages/Search.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-
-import { useParams } from "react-router-dom";
-import tmdbApi from "../api/tmdbApi";
-
-import { category } from "../api/tmdbApi";
-import Input from "../components/input/Input";
-
 import MovieCard from "../components/movie-card/MovieCard";
-import MovieList from "../components/movie-list/MovieList";
 import apiConfig from "../api/apiConfig";
+import { useLocation } from 'react-router-dom';
+import './search.scss';
+const Search = () => {
+  const location = useLocation();
+  const movies = location.state.movies;
+  const value = location.state.value;
 
-const Search = (props) => {
-    const [movies, setMovies] = React.useState([]);
+  document.title = `Search results for ${value} - ZillaXR`;
+   // const { keyword } = useParams();
+    //const [movies, setMovies] = React.useState([]);
     const [searchValue, setSearchValue] = React.useState('');
 
     const getMoviesResult = async (searchValue) => {
         if (searchValue) {
-            const url = `https://api.themoviedb.org/3/search/${category}?query=${searchValue}&api_key=${apiConfig.apiKey}`;
+            const url = `https://api.themoviedb.org/3/search/multi?query=${searchValue}&api_key=${apiConfig.apiKey}`;
             const response = await fetch(url);
             const data = await response.json();
             console.log(data);
@@ -28,24 +27,17 @@ const Search = (props) => {
             setMovies([]);
         }
     };
-
     React.useEffect(() => {
         getMoviesResult(searchValue);
     }, [searchValue]);
 
     return (
-        <div>
-            <h1>Search for {searchValue}</h1>
-            <Input
-                type="search"
-                placeholder="Search..."
-                value={searchValue}
-                onChange={(e) => setSearchValue(e.target.value)}
-            />
-            <div className="movie-grid">
+        <div className="searchcontainer">
+            <h1 className="searchtitle">SHOWING RESULTS FOR "{value}"</h1>
+            <div className="movie-gridk">
                 {
                     movies.map((item, i) => (
-                        <MovieCard category={props.category} item={item} key={i} />
+                        <MovieCard category={item.media_type} item={item} key={i} />
                     ))}
             </div>
         </div>
