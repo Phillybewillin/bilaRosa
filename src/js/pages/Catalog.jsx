c189040f4ee93b3b6cbe96405520167e032b05b7
phillybewillin <markphiloyaro27@gmail.com>
2024-09-05 18:27:22 +0300

    Update Catalog.jsx

--- src/js/pages/Catalog.jsx
+++ src/js/pages/Catalog.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 import { useParams } from 'react-router';
 
-import PageHeader from '../components/page-header/PageHeader';
-
-import { category as cate, movieType } from '../api/tmdbApi';
 import MovieGrid from '../components/movie-grid/MovieGrid';
 
 const Catalog = () => {
@@ -13,11 +10,8 @@ const Catalog = () => {
 
     return (
         <>
-            <PageHeader>
-                {category === cate.movie ? 'Movies' : 'TV Series'}
-            </PageHeader>
-            <div className="container">
-                <div className="section mb-3">
+            <div className="containerr">
+                <div className="sect">
                     <MovieGrid category={category}/>
                 </div>
             </div>
