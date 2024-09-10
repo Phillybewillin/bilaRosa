00412ecbc51524c59efcc7293fe4aebc4c428f23
phillybewillin <markphiloyaro27@gmail.com>
2024-09-10 15:19:05 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import compression from 'vite-plugin-compression2';
 import { VitePWA } from "vite-plugin-pwa";
+import purgeCss from '@fullhuman/postcss-purgecss';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, new URL('.', import.meta.url).pathname);
 
   return {
     base: env.VITE_BASE_URL || '/', 
     plugins: [
+      purgeCss({
+        content: [
+          './src/**/*.jsx',
+          './src/**/*.js',
+          './src/**/*.scss',
+        ],
+        css: ['./src/**/*.scss'],
+      }),
+      compression(),
       react(),
       VitePWA({
         disable: env.VITE_PWA_ENABLED !== "true",
