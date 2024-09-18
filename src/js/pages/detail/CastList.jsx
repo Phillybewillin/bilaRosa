f1298b90044f15b0a0e60a14d91e15e1e6065d21
phillybewillin <markphiloyaro27@gmail.com>
2024-09-18 17:45:58 +0300

    Update CastList.jsx

--- src/js/pages/detail/CastList.jsx
+++ src/js/pages/detail/CastList.jsx
@@ -16,7 +16,7 @@ const CastList = (props) => {
             try {
                 const res = await tmdbApi.credits(category , props.id);
                 setCast(res.cast.slice(0 , 15));
-                //console.log(res.cast.slice(0 , 15));
+               // console.log(res.cast.slice(0 , 15));
             } catch (error) {
                 console.error(error);
             }
@@ -31,11 +31,15 @@ const CastList = (props) => {
              casts && casts.map((item , i) => (
                  item.profile_path && (
                      <div key={i} className="casts__item">
-                         <div className="casts__item__img" loading="lazy" style={{ backgroundImage: `url(${apiConfig.w500Image(item.profile_path)})` }}>
-                             <h6 className="casts__item__char">{item.character}</h6>
-                         </div>
-                         <p className="casts__item__name">{item.name}</p>
+                         <div className="casts__item__img" loading="lazy" style={{ backgroundImage: `url(${apiConfig.w200Image(item.profile_path)})` }}>
+                        </div>
+                        <div className="aligners">
+                         
+                        <h6 className="casts__item__name">{item.name}</h6>
+                        <h6 className="casts__item__char"> as {item.character}</h6>
                       
+                        </div>
+                       
                      </div>
                  )
              ))
