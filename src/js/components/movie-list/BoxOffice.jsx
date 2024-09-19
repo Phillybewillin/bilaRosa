dcf2407c722c5f7e000fd04cb6878b3e617eac04
phillybewillin <markphiloyaro27@gmail.com>
2024-09-19 17:28:36 +0300

    Box office update

---
+++ src/js/components/movie-list/BoxOffice.jsx
@@ -0,0 +1,220 @@
+import { useEffect, useState } from "react";
+import "./movie-list.scss";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import apple from '../../assets/apple.png';
+import netflix from '../../assets/netflix.png';
+import prime from '../../assets/prime.png';
+import fx from '../../assets/fx.png';
+import hbo from '../../assets/hbo.png';
+import hulu from '../../assets/hulu.png';
+import disney from '../../assets/disney.png';
+import max from '../../assets/max.png';
+const BoxOffice = () => {
+    const navigate = useNavigate();
+
+    const [shows, setShows] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [showCount, setShowCount] = useState(10);
+    const [networkid ,setNetworkid] = useState(213);
+    const [nettitle, setNettitle] = useState('Netflix');
+    // Your TMDB API key
+    const API_KEY = '861e1d76c42a3a6e9038e0bf6dc95277'; 
+    //const NETWORK_ID = 213; // Netflix network ID
+  
+    useEffect(() => {
+      // Function to fetch TV shows from TMDB
+      const fetchNetflixShows = async () => {
+        try {
+          const response = await axios.get(
+            `https://api.themoviedb.org/3/discover/tv`,
+            {
+              params: {
+                api_key: API_KEY,
+                with_networks: networkid,
+                sort_by: "popularity.desc", // Sort by popularity
+              },
+            }
+          );
+          setShows(response.data.results); 
+          //console.log(response.data.results); // Set the TV shows
+          setLoading(false);
+        } catch (err) {
+          setError(err.message);
+          setLoading(false);
+        }
+      };
+  
+      fetchNetflixShows();
+    }, [networkid , nettitle]);
+   
+
+   
+    // Display loading, error, or the list of shows
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+  
+
+    return (
+        <div className="mainbox">
+            <div className="boxoffice">
+            <h3 className="boxoffice__title">TOP 10 BOX OFFICE</h3>
+            <div className="boxoffice__list">
+            
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/917496')}><span className="boxoffice__rank1">1</span>Beetleiuice BeetleJuice</h4>
+                 <div className="boxofficer">
+                    <p className="boxoffice__rt">77%</p>
+                    <p className="boxoffice__rating">PG13</p>
+                </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/1114513')}><span className="boxoffice__rank1">2</span> Speak No Evil</h4>
+                
+                <div className="boxofficer">
+                <p className="boxoffice__rt">83%</p>
+                <p className="boxoffice__rating">R</p>
+               
+                </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/533535')}> <span className="boxoffice__rank1">3</span>Deadpool & Wolverine</h4>
+                 <div className="boxofficer">
+                 <p className="boxoffice__rt">78%</p>
+                 <p className="boxoffice__rating">R</p>
+                 </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/1320637')}><span className="boxoffice__rank">4</span>Am I Racist</h4>
+                  <div className="boxofficer">
+                  <p className="boxoffice__rt">-</p>
+                <p className="boxoffice__rating">R</p>
+              
+                  </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/647245')}><span className="boxoffice__rank">5</span>Reagan</h4>
+                 <div className="boxofficer">
+                 <p className="boxoffice__rt">17%</p>
+                <p className="boxoffice__rating">PG13</p>
+              
+                </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/507241')}><span className="boxoffice__rank">6</span>The Killer Game</h4>
+                
+                <div className="boxofficer">
+                <p className="boxoffice__rt">39%</p>
+                <p className="boxoffice__rating">R</p>
+
+                </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/945961')}><span className="boxoffice__rank">7</span>Alien: Romulus</h4>
+                  <div className="boxofficer">
+                  <p className="boxoffice__rt">80%</p>
+                <p className="boxoffice__rating">R</p>
+              
+                  </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/1079091')}><span className="boxoffice__rank">8</span>It ends With Us</h4>
+                  <div className="boxofficer">
+                  <p className="boxoffice__rt">57%</p>
+                <p className="boxoffice__rating">PG13</p>
+              
+                  </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/1186532')}><span className="boxoffice__rank">9</span>The Forge</h4>
+                 <div className="boxofficer">
+                 <p className="boxoffice__rt">73%</p>
+                <p className="boxoffice__rating">PG</p>
+               
+                 </div>
+            </div>
+            <div className="boxoffice__list">
+                <h4 className="boxoffice__movie" onClick={() => navigate('/movie/933260')}><span className="boxoffice__rank">X</span>The Substance</h4>
+                 <div className="boxofficer">
+                 <p className="boxoffice__rt">77%</p>
+                 <p className="boxoffice__rating">PG</p>
+              
+                 </div>
+            </div>
+        </div>
+        <div className="boxoffice">
+            <div className="settitle">
+            <h4 className="boxoffice__titlez">{nettitle}</h4>
+            <h3 className="boxoffice__title">POPULAR STREAMING</h3>
+           
+            </div>
+            <div className="watchprov">
+  <h5
+    className={`netflix ${networkid === 213 ? 'activenet' : ''}`}
+    onClick={() => {setNetworkid(213); setNettitle('Netflix')}}
+  >
+    <img className="provimg" src={netflix} />
+  </h5>
+  <h5
+    className={`apple ${networkid === 2552 ? 'activenet' : ''}`}
+    onClick={() => {setNetworkid(2552); setNettitle('Apple TV+')}}
+  >
+    <img className="provimg" src={apple} />
+  </h5>
+  <h5
+    className={`hbo ${networkid === 49 ? 'activenet' : ''}`}
+    onClick={() => {setNetworkid(49); setNettitle('HBO Max')}}
+  >
+    <img className="provimg" src={hbo} />
+  </h5>
+  <h5
+    className={`hulu ${networkid === 453 ? 'activenet' : ''}`}
+    onClick={() => {setNetworkid(453); setNettitle('Hulu')}}
+  >
+    <img className="provimg" src={hulu} />
+  </h5>
+  <h5
+    className={`max ${networkid === 6783 ? 'activenet' : ''}`}
+    onClick={() => {setNetworkid(6783); setNettitle('Max')}}
+  >
+    <img className="provimg" src={max} />
+  </h5>
+  <h5
+    className={`fx ${networkid === 88 ? 'activenet' : ''}`}
+    onClick={() => {setNetworkid(88); setNettitle('FX Network')}}
+  >
+    <img className="provimg" src={fx} />
+  </h5>
+  <h5
+    className={`prime ${networkid === 1024 ? 'activenet' : ''}`}
+    onClick={() => {setNetworkid(1024); setNettitle('Amazon Prime Video')}}
+  >
+    <img className="provimg" src={prime} />
+  </h5>
+  <h5
+    className={`disney ${networkid === 2739 ? 'activenet' : ''}`}
+    onClick={() => {setNetworkid(2739); setNettitle('Disney+')}}
+  >
+    <img className="provimg" src={disney} />
+  </h5>
+</div>
+            <div className="boxoffice__lis">
+  <ul>
+    {shows.slice(0, showCount).map((show) => (
+      <li key={show.id} className="boxoffice__list">
+        <h4 className="boxoffice__movie" onClick={() => navigate(`/tv/${show.id}`)}><span className="boxoffice__rank">{show.number}</span>{show.name}</h4>
+        <p className="boxofficer">{show.vote_average.toFixed(1)}</p>
+      </li>
+    ))}
+  </ul>
+  {showCount < shows.length && (
+    <button className="boxoffice__button" onClick={() => setShowCount(showCount + 10)}>Show More</button>
+  )}
+</div>
+        </div>
+        </div>
+        
+    )
+}
+
+export default BoxOffice
