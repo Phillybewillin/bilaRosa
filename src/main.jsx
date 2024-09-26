c02dc16e9932350bc1945d82f3fead5dcedb1f4c
phillybewillin <markphiloyaro27@gmail.com>
2024-09-26 18:00:26 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,5 +12,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
         </BrowserRouter>
     </ErrorBoundary>
+    
   </React.StrictMode>,
 )
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+     // console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(err => {
+     // console.log('Service Worker registration failed:', err);
+    });
+  });
+}
+
