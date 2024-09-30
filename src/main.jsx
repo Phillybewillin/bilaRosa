17c4e27f89c099790c4585802250d6c6134ba594
phillybewillin <markphiloyaro27@gmail.com>
2024-09-30 19:10:39 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import ErrorBoundary from './js/pages/Errorboundary.jsx';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+     // console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(err => {
+     // console.log('Service Worker registration failed:', err);
+    });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
