c9b443725825cd1f467b45390006703650d25203
phillybewillin <markphiloyaro27@gmail.com>
2024-10-14 19:01:59 +0300

    Update Player.jsx

--- src/js/pages/player/Player.jsx
+++ src/js/pages/player/Player.jsx
@@ -173,7 +173,7 @@ const handleSeasonClick = (seasonNumber) => {
           <iframe
             className="episodes__iframe"
            
-            src= {season_number && episode_number ? `https://embed.su/embed/tv/${id}/${currentSeason}/${currentEpisode}` : `https://embed.su/embed/movie/${id}`}
+            src= {season_number && episode_number ? `https://vidlink.pro/tv/${id}/${currentSeason}/${currentEpisode}?poster=true&autoplay=false&nextbutton=true` : `https://vidlink.pro/movie/${id}?poster=true&autoplay=false`}
             width={"100%"}
             height={"100%"}
             frameBorder="0"
@@ -232,5 +232,3 @@ const handleSeasonClick = (seasonNumber) => {
     </ErrorBoundary>
   );
 }
-
-
