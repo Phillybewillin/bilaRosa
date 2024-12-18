a74570f190da8dc01b7bd1acefdb59bf67217dd8
phillybewillin <markphiloyaro27@gmail.com>
2024-12-18 10:53:36 +0300

    Update AuthContext.jsx

--- src/js/context/AuthContext.jsx
+++ src/js/context/AuthContext.jsx
@@ -1,13 +1,34 @@
 import { createContext,useContext,useState,useEffect } from "react";
 import { auth , db } from "../Firebase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut, onAuthStateChanged , GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
+//import { getAuth } from "firebase/auth";
+
+import PropTypes from 'prop-types';
 
 const AuthContext = createContext();
-
 export function AuthContextProvider({ children }) {
     
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    function signInwithGoogle() {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider);
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser);
+            }
+             else {
+                setUser(null);
+             };
+             setIsLoading(false);
+            
+        })
+      }, [user]);
 
     function signUp(email, password) {
         createUserWithEmailAndPassword(auth, email, password);
@@ -23,24 +44,17 @@ export function AuthContextProvider({ children }) {
         return signOut(auth);
     }
 
-    useEffect(() => {
-
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-        })
-        return () => {
-            unsubscribe();
-        }
-    })
 
    
 
     return(
-        <AuthContext.Provider value={{signUp ,logIn, logOut, user}}>
+        <AuthContext.Provider value={{signUp ,logIn, logOut, user , isLoading, signInwithGoogle}}>
             {children}
         </AuthContext.Provider>
     )
 }
+
+
 
 export function UserAuth() {
 
