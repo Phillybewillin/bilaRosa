2a2a992a737f344e43993e7e460eec155f327610
phillybewillin <markphiloyaro27@gmail.com>
2025-01-28 01:38:17 +0300

    Update Mlist.jsx

--- src/js/components/header/Mlist.jsx
+++ src/js/components/header/Mlist.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React ,{ useState } from "react";
 import apiConfig from "../../api/apiConfig";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import './mlist.scss';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { UserAuth } from '../../context/AuthContext';
+import { useFirestore } from '../../Firestore';
+import { toast } from "react-toastify";
+//import { category } from "../../api/tmdbApi";
 const Mlist = ({ movies , value }) => {
-  const [showMovies, setShowMovies] = React.useState(true);
-  
-
+  const [showMovies, setShowMovies] = useState(true);
+  const [selectedMovie, setSelectedMovie] = useState(movies[0]);
+  const {user} = UserAuth();
+  const { addToWatchlist, checkIfInWatchlist} = useFirestore();
+  const [ saved ,setSaved] = useState(false);
+  //console.log(selectedMovie);
   const navigate = useNavigate();
   
   
-  const handleClick = (type, id) => {
-    navigate(`/${type}/${id}`);
+  const handleClickza = (selectedMovie) => {
+    //console.log('handlePlayer function called', selectedMovie.id, selectedMovie.media_type);
+    const titlechange = selectedMovie.title || selectedMovie.name;
+    const encodedTitle = encodeURIComponent(titlechange.replace(/ /g, '-').toLowerCase());
+    //console.log(`Navigating to: /watch/${encodedTitle}/${id}/${selectedSeason}/${episodeNumber}`);
+   
+    if (selectedMovie.media_type === 'tv') {
+        navigate(`/watch/${encodedTitle}/${selectedMovie.id}/1/1`);
+  
+    } else {
+      navigate(`/watch/${encodedTitle}/${selectedMovie.id}`);
+     }
+    
 
     
   };
@@ -26,6 +44,42 @@ const Mlist = ({ movies , value }) => {
   useEffect(() => {
     setShowMovies(true);
   }, [movies]);
+
+  const saveShow = async (movies) => {
+        if (!user) {
+          toast.error('Access denied. Please logIn to add this to your Watchlist');
+          return;
+        }
+    
+        const data = {
+          id: movies?.id,
+          title: movies?.title || movies?.name,
+          category: movies?.media_type,
+          poster_path: movies?.poster_path,
+          release_date: movies?.release_date || movies?.first_air_date,
+          vote_average: movies?.vote_average,
+          //overview: details?.overview,
+        };
+    
+        const dataId = movies?.id?.toString();
+        await addToWatchlist(user?.uid, dataId, data);
+        const isSetToWatchlist = await checkIfInWatchlist(user?.uid, dataId);
+        setSaved(isSetToWatchlist);
+      };
+    
+      useEffect(() => {
+        if (!user) {
+          setSaved(false);
+          return;
+        }
+    
+        checkIfInWatchlist(user?.uid, movies?.id).then((data) => {
+          setSaved(data);
+          //console.log(saved)
+        });
+        
+        
+      }, [selectedMovie , user, checkIfInWatchlist ]);
  
   const handlecardClick = (id, category , title, poster_path) => {
     let continueWatching = JSON.parse(localStorage.getItem('ContinueWatching')) || [];
@@ -43,24 +97,70 @@ const Mlist = ({ movies , value }) => {
 
   return (
     <>
+    <div className="containermodz">
+        <div className="modalsearchcontent">
+       
+        <div className="detaizza">
+        <div className="background-image" style={{
+    backgroundImage: `url(${apiConfig.w200Image(selectedMovie.poster_path)})`,
+    
+  }} />  
+        <h2 className="movietitleza">{selectedMovie.title || selectedMovie.name}</h2>
+        <div className="date">
+                <p className="ratingz">
+                  {selectedMovie.media_type === "tv" ? "Show" : "Movie"}
+                </p>
+                {selectedMovie.vote_average && (
+                  <p className="ratingz">{selectedMovie.vote_average.toFixed(1)}</p>
+                )}
+                <p className="ratingz">
+                  {(new Date(selectedMovie.release_date || selectedMovie.first_air_date))
+                    .getFullYear()}
+                </p>
+                <p className="ratingz">
+                 {selectedMovie.original_language.toUpperCase()}
+                </p>
+          </div>
+          <h3 className="movietitlesum">~ SUMMARY</h3>
+          <p className="overviewseries">"{selectedMovie.overview}"</p>
+           <div className="showwet">
+             <button className="shobuttn2" onClick={() => handlecardClick(selectedMovie.id,selectedMovie.category || selectedMovie.media_type, selectedMovie.title || selectedMovie.name, selectedMovie.poster_path,)}><i className='bx bx-info-circle'></i></button>
+            <div className="szz">
+            {
+                                        saved ? (
+                                            <div className="languagezz" onClick={() => setSaved(false)}><i class='bx bxs-add-to-queue' style={{fontSize:'17px'}} ></i> Added To Watchlist</div>
+                               
+                                        ):(
+                                            <div className="languagez" onClick={() => saveShow(selectedMovie)}><i class='bx bx-add-to-queue' style={{fontSize:'17px'}} ></i> Add To Watchlist</div>
+                              
+                                        )
+               }
+              </div>   
+              <button className="shobuttn" onClick={() => handleClickza(selectedMovie)}>
+              <i className='bx bx-play'></i> Watch
+            </button>
+                          
+           </div>
+          
+        
+        </div>
+            
+      </div>
+    </div>
+    
       {showMovies && (
         <>
         <div className="moviez" onClick={handleImageClick}>
           {movies && movies.map((movie, index) => (
             <div className="movieimg" key={index} onClick={() => handlecardClick(movie.id,movie.category || movie.media_type, movie.title || movie.name, movie.poster_path,)} >
               <img
-                onClick={() => handleClick(movie.media_type, movie.id , movie.title || movie.name)}
-                style={{ padding: "2px", height: "110px", width: "80px", objectFit: "cover", borderRadius: "10px" }}
-                src={movie?.poster_path ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : ""}
-                 alt=""
+                onMouseEnter={(e) => setSelectedMovie(movie) }
+                onMouseLeave={(e) => setSelectedMovie(movie)}
+                className="moviezabs"
+                 src={movie?.poster_path ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : ""}
+                 alt={movie.title || movie.name}
               />
-               <div className="dets">
-               <p className="movietitlez">{movie.title || movie.name}</p>
-              <p className="overviewseries">{movie.overview}</p>
-              <div className="date"> <p className="ratingz">• {(new Date(movie.release_date || movie.first_air_date)).getFullYear()}</p><p className="type">{movie.media_type === 'tv' ? 'Show' : 'Movie'}</p> {movie.vote_average && <p className="ratingz">{movie.vote_average.toFixed(1)}</p>} 
-              </div>
-                
-              </div>
+              
             </div>
           ))}
         </div>
