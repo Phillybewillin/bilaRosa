1aad3ee083cab30ec1044191cb8a95d0a424ed50
phillybewillin <markphiloyaro27@gmail.com>
2025-03-26 00:35:32 +0300

    Update MovieCard.jsx

--- src/js/components/movie-card/MovieCard.jsx
+++ src/js/components/movie-card/MovieCard.jsx
@@ -103,7 +103,7 @@ const MovieCard = React.memo((props) => {
         setIsLoading(false);
         setBg(apiConfig.w500Image(item.poster_path )); // Set the bg state variable using the apiConfig.w500Image function
       };
-      img.src = apiConfig.w500Image(item.poster_path);
+      img.src = apiConfig.w200Image(item.poster_path);
     }, [item.poster_path]);
 
     const voteAverage = item.vote_average; // example vote average
@@ -111,14 +111,15 @@ const MovieCard = React.memo((props) => {
 
     const getColor = (votePercentage) => {
       if (votePercentage >= 86) {
-          return 'magenta';
+        return '#9b59b6'; // Royal Purple for the best of the best
       } else if (votePercentage >= 70) {
-          return 'rgb(9, 255, 0)';
-      } 
-      else if (votePercentage >= 55) {
-          return 'yellow';
+        return '#2ecc71'; // Emerald Green for 70 and above
+      } else if (votePercentage >= 60) {
+        return '#f1c40f'; // Sunflower Yellow for 60 and above
+      } else if (votePercentage >= 55) {
+        return '#e67e22'; // Carrot Orange for 55 and above
       } else {
-        return 'red';
+        return '#e74c3c'; // Red for lower ratings
       }
           
      
@@ -132,7 +133,7 @@ const MovieCard = React.memo((props) => {
         
         {isLoading ? (
           <SkeletonTheme  baseColor='#ffffff11' enableAnimation={false} >
-            <Skeleton borderRadius={20} className="movie-card" style={{background: 'linear-gradient(120deg, rgba(0, 28, 28, 0.7), rgba(66, 0, 62, 0.7))' ,margin :'0 5px' , height : '100%' , width : '180px'}} customHighlightBackground = 'linear-gradient(120deg, rgba(0, 28, 0, 0.7), rgba(66, 0, 13, 0.7))' variant="rect" />
+            <Skeleton borderRadius={20} className="movie-card" style={{background: 'linear-gradient(120deg, rgba(20, 20, 20, 0.7), rgba(91, 81, 0, 0.38))' ,margin :'0 5px' , height : '270px' , width : '190px'}} customHighlightBackground = 'linear-gradient(120deg, rgba(0, 28, 0, 0.7), rgba(66, 0, 13, 0.7))' variant="rect" />
           </SkeletonTheme>
         ) : (
          
@@ -140,22 +141,25 @@ const MovieCard = React.memo((props) => {
             <div className="inCinema">
               <i className='bx bx-badge-check' style={{fontSize :'12px'}}></i> { cinemaStatus}
             </div>
-            <div className="catz">{props.category === 'tv' ? 'Show' : 'Movie'}</div> 
-            <div className="btnz">
+             <div className="btnz">
             <i className='bx bx-play-circle'></i>
             </div>
+            <h4 className='year'>
+            {Number.isNaN(year) ? '' : year}
+           </h4>
+              
             <div className="infomovie">
             <div className="title">
               <div className="titlename">
                 {item.title || item.name }
               </div> 
             </div>
+            <div className="vote" style={{color: getColor(votePercentage.toFixed(0))}}>{votePercentage.toFixed(0)}%</div> 
+          
             <div className="cat"> 
-            <h4 className='year'>
-            {Number.isNaN(year) ? '' : year}
-           </h4>
-                <div className="vote" style={{color: getColor(votePercentage.toFixed(0))}}>{votePercentage.toFixed(0)}%</div> 
-                <button className="savemovie" onClick={saveShow}>    <p style={{ cursor : 'pointer' , color : isInWatchlist ? 'aqua' : 'rgba(255, 255, 255, 0.549)'}}>
+            <div className="catz">{props.category === 'tv' ? 'Show' : 'Movie'}</div> 
+          
+            <button className="savemovie" onClick={saveShow}>    <p style={{ cursor : 'pointer' , color : isInWatchlist ? 'aqua' : 'rgba(255, 255, 255, 0.549)'}}>
           {isInWatchlist ? <i className='bx bxs-bookmark-plus'  style={{fontSize :'19px'}}></i> :<i className='bx bx-bookmark-plus' style={{fontSize :'18px'}}></i>}
            </p>
               </button>
