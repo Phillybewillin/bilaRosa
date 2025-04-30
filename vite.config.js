246905d64e4d687eec48d7e9b36b6b233e580eeb
phillybewillin <markphiloyaro27@gmail.com>
2025-04-30 23:01:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,71 +1,61 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import compression from 'vite-plugin-compression2';
-import { VitePWA } from "vite-plugin-pwa";
-import purgeCss from '@fullhuman/postcss-purgecss';
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, new URL('.', import.meta.url).pathname);
 
   return {
-    base: '/', 
+    base: '/',
     plugins: [
-      purgeCss({
-        content: [
-          './src/**/*.jsx',
-          './src/**/*.js',
-          './src/**/*.scss',
-        ],
-        css: ['./src/**/*.scss'],
-      }),
       compression(),
       react(),
       VitePWA({
-        disable: env.VITE_PWA_ENABLED !== "true",
-        registerType: "autoUpdate",
+        disable: env.VITE_PWA_ENABLED !== 'true',
+        registerType: 'autoUpdate',
         includeAssets: [
-        
-          "favicon.ico",
-          "apple-touch-icon.png",
-          "safari-pinned-tab.svg",
-          "favicon-32x32.png",
-          "favicon-16x16.png",
-          "sitemap.xml",
-
-         
+          'favicon.ico',
+          'apple-touch-icon.png',
+          'safari-pinned-tab.svg',
+          'favicon-32x32.png',
+          'favicon-16x16.png',
+          'sitemap.xml',
         ],
         manifest: {
-          id: "https://zilla-xr.xyz",
-          name: "ZillaXR",
-          short_name: "ZillaXR",
-          description: "Stream a wide range of Movies and Series up to 4k for Free, Watch Movies online without interuptions and without ads .",
-          background_color: "#000000",
-          theme_color: "#000000",
-          display: "standalone",
-          start_url: "/",
+          id: 'https://moviepluto.fun',
+          name: 'MoviePluto',
+          short_name: 'MoviePluto',
+          description: 'Stream  Movies and Series in 4K for Free',
+
+          background_color: '#000000',
+          theme_color: '#000000',
+          display: 'standalone',
+          start_url: '/',
           icons: [
             {
-              src: "android-chrome-192x192.png",
-              sizes: "192x192",
-              type: "image/png",
-              purpose: "any",
+              src: 'android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any',
             },
             {
-              src: "android-chrome-512x512.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "any",
+              src: 'android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any',
             },
             {
-              src: "android-chrome-192x192.png",
-              sizes: "192x192",
-              type: "image/png",
-              purpose: "maskable",
+              src: 'android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable',
             },
             {
-              src: "android-chrome-512x512.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "maskable",
+              src: 'android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
             },
           ],
         },
@@ -80,7 +70,6 @@ export default defineConfig(({ mode }) => {
             @use './src/js/scss/_index.scss' as *;
             @use './src/js/scss/_breakpoint.scss' as *;
             @use './src/js/scss/_mixin.scss' as *;
-           
           `,
         },
       },
@@ -89,11 +78,11 @@ export default defineConfig(({ mode }) => {
       minify: 'esbuild',
       rollupOptions: {
         output: {
-  manualChunks: {
-    vendor: ['react', 'react-dom'],
-    swiper: ['swiper'],
-  },
-},
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+            swiper: ['swiper'],
+          },
+        },
       },
     },
   };
