61f9686612538989eff01f79ec972ce8e9252a30
phillybewillin <markphiloyaro27@gmail.com>
2025-05-18 03:11:57 +0300

    Update CastList.jsx

--- src/js/pages/detail/CastList.jsx
+++ src/js/pages/detail/CastList.jsx
@@ -1,51 +1,48 @@
-import React , { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import tmdbApi from "../../api/tmdbApi";
 import apiConfig from "../../api/apiConfig";
 import './detail.scss';
 
-const CastList = (props) => {
+const MAX_CASTS = 15;
 
-    const {category} = useParams();
+const CastList = ({ id }) => {
+  const { category } = useParams();
+  const [casts, setCast] = useState([]);
 
-    const [casts , setCast] = useState([]);
+  useEffect(() => {
+    const fetchCredits = async () => {
+      try {
+        const res = await tmdbApi.credits(category, id);
+        setCast(res.cast.slice(0, MAX_CASTS));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCredits();
+  }, [category, id]);
 
-    useEffect(() => {
-
-        const getCredits = async () => {
-            try {
-                const res = await tmdbApi.credits(category , props.id);
-                setCast(res.cast.slice(0 , 15));
-               // console.log(res.cast.slice(0 , 15));
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        getCredits();
-    },[category , props.id]);
-
-    
-    return (
-      <div className="casts">
-         {
-             casts && casts.map((item , i) => (
-                 item.profile_path && (
-                     <div key={i} className="casts__item">
-                         <div className="casts__item__img" loading="lazy" style={{ backgroundImage: `url(${apiConfig.w200Image(item.profile_path)})` }}>
-                        </div>
-                        <div className="aligners">
-                         
-                        <h6 className="casts__item__name">{item.name}</h6>
-                        <h6 className="casts__item__char"> as {item.character}</h6>
-                      
-                        </div>
-                       
-                     </div>
-                 )
-             ))
-         }
-      </div>
+  return (
+    <div className="casts">
+      {casts &&
+        casts.map((item, i) =>
+          item.profile_path ? (
+            <div key={i} className="casts__item">
+              <img
+                className="casts__item__img"
+                loading="lazy"
+                src={apiConfig.w200Image(item.profile_path)}
+                alt={item.name}
+              />
+              <div className="aligners">
+                <h6 className="casts__item__name">{item.name}</h6>
+                <h6 className="casts__item__char">as {item.character}</h6>
+              </div>
+            </div>
+          ) : null
+        )}
+    </div>
   );
-}
+};
 
 export default CastList;
